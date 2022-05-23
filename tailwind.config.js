fd8a6b1b679730231de55510577e4463b3ac4b49
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-23 21:58:45 +0700

    finish CTA

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./src/**/*.{html,js}",
     "./dist/**/*.{html,js}"
   ],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
   theme: {
     extend: {
       colors: {
